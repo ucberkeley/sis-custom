152931b55b9252ed9cd8ebe90640adfd935ab9ef
Christian Vuerings <vueringschristian@gmail.com>
2015-07-15 19:23:45 -0700

    SISRP-5495 - The CSS doesn't load as the last file on CFG
    https://jira.berkeley.edu/browse/SISRP-5495

--- src/js/loadfiles/loadcss.js
+++ src/js/loadfiles/loadcss.js
@@ -4,7 +4,7 @@
   /**
    * Slightly modified version of https://git.io/vtlVO
    */
-  var loadCSS = function(href, before, media, callback) {
+  var loadCSS = function(href, id) {
     // Arguments explained:
     // `href` is the URL for your CSS file.
     // `before` optionally defines the element we'll use as a reference for injecting our <link>
@@ -12,46 +12,40 @@
     // However, since the order in which stylesheets are referenced matters, you might need a more specific location in your document.
     // If so, pass a different reference element to the `before` argument and it'll insert before that instead
     // note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
-    var ss = window.document.createElement('link');
     var scripts = window.document.head.getElementsByTagName('script');
     var lastScript = scripts[scripts.length - 1];
-    var ref = before || lastScript;
-    var sheets = window.document.styleSheets;
-    ss.rel = 'stylesheet';
-    ss.href = href;
-    // temporarily, set media to something non-matching to ensure it'll fetch without blocking render
-    ss.media = 'only x';
-    // DEPRECATED
-    if (callback) {
-      ss.onload = callback;
+
+    var ss;
+    var previousElement = document.getElementById(id);
+
+    // Check whether there is a CSS file that has been added before.
+    // If so, don't add it again, but move it to the last element in the HEAD
+    if (previousElement) {
+      ss = previousElement;
+    } else {
+      ss = window.document.createElement('link');
+      ss.rel = 'stylesheet';
+      ss.href = href;
+      ss.id = id;
+      ss.media = 'all';
     }
 
     // inject link
-    ref.parentNode.insertBefore(ss, ref);
-    // This function sets the link's media back to `all` so that the stylesheet applies once it loads
-    // It is designed to poll until document.styleSheets includes the new sheet.
-    ss.onloadcssdefined = function(cb) {
-      var defined;
-      for (var i = 0; i < sheets.length; i++) {
-        if (sheets[ i ].href && sheets[ i ].href === ss.href) {
-          defined = true;
-        }
-      }
-      if (defined) {
-        cb();
-      } else {
-        setTimeout(function() {
-          ss.onloadcssdefined(cb);
-        });
-      }
-    };
-    ss.onloadcssdefined(function() {
-      ss.media = media || 'all';
-    });
+    lastScript.parentNode.insertBefore(ss, lastScript);
     return ss;
   };
 
   // Random number for cache busting
   var randomNumber = Math.round(Math.random() * 10000000000000);
-  loadCSS('/uc_cust/data/portal/css/sis_cs.css' + '?_=' + randomNumber);
+
+  // Count how many times we try to add the CSS
+  var counter = 0;
+  var interval = setInterval(function() {
+    counter++;
+    // We do this 50 times just to make sure the CSS is always the last CSS on the page
+    if (counter === 50) {
+      clearInterval(interval);
+    }
+    loadCSS('/uc_cust/data/portal/css/sis_cs.css' + '?_=' + randomNumber, 'sis_cs_css');
+  }, 100);
 })();
--- src/js/loadfiles/loadjs.js
+++ src/js/loadfiles/loadjs.js
@@ -4,19 +4,23 @@
   /**
    * Slightly modified version of https://git.io/vtlVH
    */
-  var loadJS = function(src, cb) {
-    var ref = window.document.getElementsByTagName('script')[ 0 ];
+  var loadJS = function(src, id) {
+    var previousElement = document.getElementById(id);
+
+    // Only add the JavaScript once to the page
+    if (previousElement) {
+      return;
+    }
+
+    var ref = window.document.getElementsByTagName('script')[0];
     var script = window.document.createElement('script');
     script.src = src;
-    script.async = true;
+    script.id = id;
     ref.parentNode.insertBefore(script, ref);
-    if (cb && typeof(cb) === 'function') {
-      script.onload = cb;
-    }
     return script;
   };
 
   // Random number for cache busting
   var randomNumber = Math.round(Math.random() * 10000000000000);
-  loadJS('/uc_cust/data/portal/js/sis_cs.js' + '?_=' + randomNumber);
+  loadJS('/uc_cust/data/portal/js/sis_cs.js' + '?_=' + randomNumber, 'sis_cs_js');
 })();
